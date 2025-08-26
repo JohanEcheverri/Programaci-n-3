16b6027c0d83a43ee84b41cd1ddd1bdbe784843a
JohanEcheverri <johans.echeverrii@uqvirtual.edu.co>
2025-08-26 15:05:47 -0500

    Entrada de reales y entero

--- util.ex
+++ util.ex
@@ -1,3 +1,4 @@
+
 defmodule Util do
   @moduledoc """
 Módulo con funciones que se reutilizan
@@ -7,27 +8,40 @@ Módulo con funciones que se reutilizan
 """
 
 @doc """
-Función para mostrar un mensaje en la pantalla.
+Función para mostrar un mensaje en la pantalla con Java
 ## Parámetro
   - mensaje: texto que se le presenta al usuario
-## Ejemplo
-    iex> Util.mostrar_mensaje("Hola Mundo")
 """
   def mostrar_mensaje_Java(mensaje) do
     System.cmd("java", ["-cp", ".", "Mensaje", mensaje])
   end
 
+  @doc """
+Función para mostrar un mensaje
+## Parámetro
+  - mensaje: texto que se le presenta al usuario
+"""
   def mostrar_mensaje(mensaje) do
     mensaje
     |>IO.puts()
   end
 
+@doc """
+Función para ingresar un String
+## Parámetros
+  - mensaje: texto que ingresa el usuario
+"""
   def ingresar(mensaje, :texto) do
   mensaje
   |> IO.gets()
   |> String.trim()
   end
 
+@doc """
+Función para ingresar un String con Java
+## Parámetro
+  - mensaje: texto que ingresa el usuario
+"""
   def ingresar_Java(mensaje,:texto) do
   # Llama al programa Java para ingresar texto y capturar la entrada
     case System.cmd("java", ["-cp", ".", "Mensaje","input", mensaje]) do
@@ -40,5 +54,54 @@ Función para mostrar un mensaje en la pantalla.
         IO.puts("Detalles: #{error}")
       nil #!null
     end
+  end
+
+@doc """
+Función para ingresar un Integer
+## Parámetro
+  - mensaje: texto que ingresa el usuario
+"""
+  def ingresar(mensaje, :entero) do
+    try do
+      mensaje
+      |> ingresar(:texto)
+        |> String.to_integer()
+  rescue
+    ArgumentError ->
+      "Error, se espera que ingrese un número entero\n"
+      |> mostrar_error()
+
+      mensaje
+      |> ingresar(:entero)
+    end
+  end
+
+@doc """
+Función para ingresar un numero real
+## Parámetro
+  - mensaje: texto que ingresa el usuario
+"""
+  def ingresar(mensaje, :real) do
+  try do
+    mensaje
+    |> ingresar(:texto)
+    |> String.to_float()
+  rescue
+    ArgumentError ->
+      "Error, se espera que ingrese un número real\n"
+      |> mostrar_error()
+
+      mensaje
+      |> ingresar(:real)
+    end
+  end
+
+@doc """
+Función para ingresar mostrar un mensaje de error
+## Parámetro
+  - mensaje: texto que de error que le aparece al usuario
+"""
+  def mostrar_error(mensaje) do
+    IO.puts(:standard_error, mensaje)
   end
 end
